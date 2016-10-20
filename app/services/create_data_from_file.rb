ad35b341bc70deafd1d1ec13375ebf73b953e52d
Kelly Munro <kellymdev@gmail.com>
2016-10-20 15:26:45 +1300

    Add name to the sign up and edit forms and get it working with devise

--- app/services/create_data_from_file.rb
+++ app/services/create_data_from_file.rb
@@ -32,11 +32,11 @@ class CreateDataFromFile
   end
 
   def create_activity(row)
-    Activity.create!(date: Date.parse(row[0]), calories_burned: convert_to_i(row[1]), steps: convert_to_i(row[2]), distance: row[3].to_d, floors: convert_to_i(row[4]), minutes_sedentary: convert_to_i(row[5]), minutes_lightly_active: convert_to_i(row[6]), minutes_fairly_active: convert_to_i(row[7]), minutes_very_active: convert_to_i(row[8]), activity_calories: convert_to_i(row[9]))
+    current_user.activities.create!(date: Date.parse(row[0]), calories_burned: convert_to_i(row[1]), steps: convert_to_i(row[2]), distance: row[3].to_d, floors: convert_to_i(row[4]), minutes_sedentary: convert_to_i(row[5]), minutes_lightly_active: convert_to_i(row[6]), minutes_fairly_active: convert_to_i(row[7]), minutes_very_active: convert_to_i(row[8]), activity_calories: convert_to_i(row[9]))
   end
 
   def create_sleep(row)
-    Sleep.create!(date: Date.parse(row[0]), minutes_asleep: convert_to_i(row[1]), minutes_awake: convert_to_i(row[2]), number_of_awakenings: convert_to_i(row[3]), time_in_bed: convert_to_i(row[4]))
+    current_user.sleeps.create!(date: Date.parse(row[0]), minutes_asleep: convert_to_i(row[1]), minutes_awake: convert_to_i(row[2]), number_of_awakenings: convert_to_i(row[3]), time_in_bed: convert_to_i(row[4]))
   end
 
   def convert_to_i(value)
